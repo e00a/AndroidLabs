fe5cf72b203b5bfd0b588d6ee998a5540f229d6a
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2023-11-04 17:02:55 +0300

    ready to pass 568

--- app/src/main/java/com/mdk0103/labwork17/screens/CallSettingsSecreen.kt
+++ app/src/main/java/com/mdk0103/labwork17/screens/CallSettingsSecreen.kt
@@ -5,69 +5,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CallSettingsScreen(modifier: Modifier = Modifier) {
-    val selectedOption = remember { mutableStateOf(CallPermissionOption.ALL) }
-
-    Column(Modifier.selectableGroup()) {
-        RadioButtonOption(
-            option = CallPermissionOption.ALL,
-            selectedOption = selectedOption.value,
-            onOptionSelected = { selectedOption.value = it }
-        )
-        RadioButtonOption(
-            option = CallPermissionOption.MY_CONTACTS,
-            selectedOption = selectedOption.value,
-            onOptionSelected = { selectedOption.value = it }
-        )
+    val options = listOf("Все", "Мои контакты")
+    var selectedOption by remember { mutableStateOf(options[0]) }
+    Column(
+        modifier = Modifier.selectableGroup()
+    ) {
+        options.forEach { option ->
+            SettingsRadioButton(option = option,
+                option == selectedOption,
+                onOptionSelected = { selectedOption = option })
+        }
     }
 }
 
 @Composable
-fun RadioButtonOption(
-    option: CallPermissionOption,
-    selectedOption: CallPermissionOption,
-    onOptionSelected: (CallPermissionOption) -> Unit
+fun SettingsRadioButton(
+    option: String, isSelected: Boolean, onOptionSelected: () -> Unit
 ) {
-    val isSelected = option == selectedOption
-    Row {
-        RadioButton(
-            selected = isSelected,
-            onClick = { onOptionSelected(option) },
-        )
+    Row(
+        verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+    ) {
+        RadioButton(selected = isSelected, onClick = onOptionSelected)
 
-        Text(
-            text = option.label,
-            modifier = Modifier.padding(start = 8.dp)
-        )
+        Text(text = option)
     }
-}
-
-
-enum class CallPermissionOption(val label: String) {
-    ALL("Все"), MY_CONTACTS("Мои контакты")
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/mdk0103/labwork17/screens/LanguageScreen.kt
+++ app/src/main/java/com/mdk0103/labwork17/screens/LanguageScreen.kt
@@ -1,97 +1,55 @@
 package com.mdk0103.labwork17.screens
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun LanguageScreen(modifier: Modifier = Modifier) {
-    val selectedLanguage = remember { mutableStateOf(LanguageOption.ENGLISH) }
-
-    Column(Modifier.selectableGroup()) {
-        RadioButtonOption(
-            option = LanguageOption.ENGLISH,
-            selectedOption = selectedLanguage.value,
-            onOptionSelected = { selectedLanguage.value = it }
-        )
-        RadioButtonOption(
-            option = LanguageOption.SPANISH,
-            selectedOption = selectedLanguage.value,
-            onOptionSelected = { selectedLanguage.value = it }
-        )
-        RadioButtonOption(
-            option = LanguageOption.FRENCH,
-            selectedOption = selectedLanguage.value,
-            onOptionSelected = { selectedLanguage.value = it }
-        )
-        RadioButtonOption(
-            option = LanguageOption.GERMAN,
-            selectedOption = selectedLanguage.value,
-            onOptionSelected = { selectedLanguage.value = it }
-        )
-        RadioButtonOption(
-            option = LanguageOption.ITALIAN,
-            selectedOption = selectedLanguage.value,
-            onOptionSelected = { selectedLanguage.value = it }
-        )
-    }
-    Text(
-        text = "Выбранный язык: ${selectedLanguage.value.label}",
-        modifier = Modifier.padding(start = 16.dp)
-    )
-}
-
-@Composable
-fun RadioButtonOption(
-    option: LanguageOption,
-    selectedOption: LanguageOption,
-    onOptionSelected: (LanguageOption) -> Unit
-) {
-    Row(Modifier.selectable(
-        selected = (option == selectedOption),
-        onClick = { onOptionSelected(option) }
-    )) {
-        RadioButton(
-            selected = (option == selectedOption),
-            onClick = { onOptionSelected(option) },
-        )
+fun LanguageScreen() {
+    val languages = listOf("English", "Español", "Français", "Deutsch", "Русский")
+    var selectedLanguage by remember { mutableStateOf(languages[0]) }
+    Column(
+        modifier = Modifier
+    ) {
+        Text(text = "Выберете язык", modifier = Modifier)
+        languages.forEach { language ->
+            LanguageItem(language = language,
+                isSelected = language == selectedLanguage,
+                onLanguageSelected = { selectedLanguage = language })
+        }
         Text(
-            text = option.label,
-            modifier = Modifier
+            text = "Selected Language: ${selectedLanguage}", modifier = Modifier
         )
     }
 }
 
-enum class LanguageOption(val label: String) {
-    ENGLISH("English"),
-    SPANISH("Spanish"),
-    FRENCH("French"),
-    GERMAN("German"),
-    ITALIAN("Italian")
+@Composable
+fun LanguageItem(
+    language: String, isSelected: Boolean, onLanguageSelected: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .selectable(selected = isSelected, onClick = onLanguageSelected)
+            .fillMaxWidth()
+    ) {
+        RadioButton(selected = isSelected, onClick = onLanguageSelected)
+
+        Text(text = language)
+    }
 }
 
 @Preview(showBackground = true)
